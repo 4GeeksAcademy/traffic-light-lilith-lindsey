d6369080b50a00cd21049147a7f04a940d3e42a9
Lilith Lindsey <lilithx1313@gmail.com>
2025-08-17 22:16:31 +0000

    created traffic light component and applied css styling

---
+++ src/js/components/trafficLight.jsx
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from "react";
+import "../../styles/index.css";
+
+
+const TrafficLight = () => {
+    return (
+        <body>
+            <div className = "lightFrame">
+                <div className = "redLight">
+                    
+                </div>
+                <div className = "yellowLight">
+                    
+                </div>
+                <div className = "greenLight">
+                    
+                </div>
+            </div>
+        </body>
+    );
+};
+
+export default trafficLight;
